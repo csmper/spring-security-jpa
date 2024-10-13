b9118500d108200016a5b6991b1c895e7765ce41
Suhas <suhas.gavas@gmail.com>
2024-10-13 22:07:01 +0530

    initial commit

---
+++ src/main/java/com/suhas/springsecurityjpa/HomeResource.java
@@ -0,0 +1,22 @@
+package com.suhas.springsecurityjpa;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+    @GetMapping("/")
+    public String home() {
+        return ("Welcome");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("Welcome User");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("Welcome Admin");
+    }
+}
---
+++ src/main/java/com/suhas/springsecurityjpa/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.suhas.springsecurityjpa;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+        return http.build();
+    }
+}
